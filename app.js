3dc871c0e5fbdb996355d77327f6b87d211c54e2
Vatsal-srivastav <vatsalsrivastav03@gmail.com>
2025-06-18 00:30:57 +0530

    Update app.js

--- app.js
+++ app.js
@@ -106,7 +106,7 @@ function makeMove(boardIndex, cellIndex) {
 }
 
 function botMove() {
-  let {move} = bot(boards, boardWinners, activeBoard, "O", 0, 25);
+  let {move} = botMinimax(boards, boardWinners, activeBoard, "O", 0, 3);
   if (move) {
     makeMove(move.bi, move.ci);
   }
